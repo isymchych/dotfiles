eecd2d443bbd123d336704eb7292eb7ba377b3a3
isymchych <ivan.symchych@gmail.com>
2021-11-04 13:56:59 +0200

    bump configs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -253,31 +253,6 @@
 
 
 
-
-;; ---------------------------------------- copy-paste in Wayland https://gist.github.com/yorickvP/6132f237fbc289a45c808d8d75e0e1fb
-(if mb-is-linux
-    (progn
-      (setq wl-copy-process nil)
-
-      (defun wl-copy (text)
-        (setq wl-copy-process (make-process :name "wl-copy"
-                                            :buffer nil
-                                            :command '("wl-copy" "-f" "-n")
-                                            :connection-type 'pipe))
-        (process-send-string wl-copy-process text)
-        (process-send-eof wl-copy-process))
-
-      (defun wl-paste ()
-        (if (and wl-copy-process (process-live-p wl-copy-process))
-            nil ; should return nil if we're the current paste owner
-          (shell-command-to-string "wl-paste -n | tr -d \r")))
-
-      (setq interprogram-cut-function 'wl-copy)
-      (setq interprogram-paste-function 'wl-paste)
-      ))
-
-
-
 ;; ---------------------------------------- UTILS
 
 
@@ -572,6 +547,14 @@ narrowed."
   :ensure t
   :config
   (mb/load-theme! 'solarized-light))
+
+
+
+;; Make clipboard work on all platforms
+(use-package xclip
+  :ensure t
+  :config
+  (xclip-mode 1))
 
 
 
