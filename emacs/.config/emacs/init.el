b5612ddc4faedd0545fbd9eaed550fe42e67f683
isymchych <ivan.symchych@gmail.com>
2022-10-23 21:25:44 +0300

    bump configs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1409,10 +1409,12 @@ targets."
    popper-display-control nil
    popper-reference-buffers
    '("\\*Messages\\*"
+     "\\*Warnings\\*"
      "\\*Apropos\\*"
      "Output\\*$"
      "\\*Async Shell Command\\*"
      "\\*Flycheck"
+     "\\*rustfmt\\*"
      "\\magit: "
      help-mode
      compilation-mode))
@@ -1796,10 +1798,6 @@ targets."
         transient-default-level 5
 
         magit-diff-refine-hunk t ; show granular diffs in selected hunk
-        ;; Don't autosave repo buffers. This is too magical, and saving can
-        ;; trigger a bunch of unwanted side-effects, like save hooks and
-        ;; formatters. Trust the user to know what they're doing.
-        magit-save-repository-buffers nil
         ;; Don't display parent/related refs in commit buffers; they are rarely
         ;; helpful and only add to runtime costs.
         magit-revision-insert-related-refs nil)
